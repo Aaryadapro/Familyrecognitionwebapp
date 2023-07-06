c68db9165210e3bfe040c47157834880d806e607
Aarya's Rep <116066096+Aaryadapro@users.noreply.github.com>
2023-07-06 18:37:37 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+Webcam.set({
+    width: 250,
+    height: 250,
+    image_format: 'png',
+    png_quality:90
+});
+            
+camera = document.getElementById("camera");
+ 
+Webcam.attach(camera);
+
+function takeSnapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+
+    });
+}
+
+console.log("ml5 version is : ", ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/aIKn6gfR6/model.json", modelLoaded);
+
+
+
+function modelLoaded(){
+    console.log("model has loaded");
+}
+
